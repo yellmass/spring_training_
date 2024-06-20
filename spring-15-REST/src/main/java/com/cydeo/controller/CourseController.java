b0b408fa561651fe918a2b96f825f0bcb725eeb1
yellmass <ebincyilmaz@gmail.com>
2024-06-20 15:30:32 -0400

    CourseController with BaseEntity

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/courses")
+@RequestMapping("/courses/api/v1")
 public class CourseController {
 
     private final CourseService courseService;
